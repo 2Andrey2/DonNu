4650ad0b9d579fd6b45de954686fa837c79a11ad
Garet <viandryvasilkov@mail.com>
2021-09-05 21:27:44 +0300

    123

--- 3_course/web_curs/lb1/Infrastructure.php
+++ 3_course/web_curs/lb1/Infrastructure.php
@@ -2,6 +2,8 @@
 <head>
     <title>Инфраструктура</title>
     <link rel="stylesheet" href="css/css.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/infrastructure.css">
 </head>
 <body>
     <header>
@@ -11,7 +13,113 @@
         <div class="logo_page">
             <h1>Инфраструктура</h1>
         <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d25765.52431114135!2d37.82638521622356!3d48.00523107760643!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1630782525257!5m2!1sru!2sua" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <div class="content2" style="display: inline-flex; justify-content: center; margin: 10px;">
+            <div style="width: 50%;">
+                <div class="post-item">
+                    <div class="post-item-wrap">
+                    <a href="" class="post-link">
+                    <h3 class="post-title">География</h3>
+                    <p style="color: #403d39; font-size: 20px;">Географические координаты Донецка — 48° северной широты и 37° 48′ восточной долготы. Часовой пояс — UTC+3 По северным и западным предместьям города проходит линия разграничения сил в Донбассе (см. Второе минское соглашение). Общая площадь Донецка — 385 км². Протяжённость города с севера на юг — 38 км. Протяжённость с востока на запад — 55 км. Вместе с близлежащими городами Донецк входит в состав Донецкой агломерации — крупнейшего индустриального узла Украины. Агломерация с населением в 1720 тыс. чел. представляет собой зону неразрывной застройки — граница между Донецком и Макеевкой проходит по улице. Город находится в центральной части Донбасса к югу от Донецкого кряжа. Донецк расположен в степной зоне, в верховьях реки Кальмиус и окружён небольшими лесами, холмами, реками и озёрами.</p>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d25765.52431114135!2d37.82638521622356!3d48.00523107760643!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1630782525257!5m2!1sru!2sua" width="500" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+        <table style="width: 80%; margin: auto;" id="schedule_table" role="grid" aria-describedby="schedule_table_info" style="width: 1143px;">
+		<thead>
+			<tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="schedule_table" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Отправление: активировать для сортировки столбца по убыванию" style="width: 93px;">Отправление</th><th class="sorting" tabindex="0" aria-controls="schedule_table" rowspan="1" colspan="1" aria-label="Маршрут: активировать для сортировки столбца по возрастанию" style="width: 247px;">Маршрут</th><th class="sorting" tabindex="0" aria-controls="schedule_table" rowspan="1" colspan="1" aria-label="Прибытие: активировать для сортировки столбца по возрастанию" style="width: 72px;">Прибытие</th><th class="sorting" tabindex="0" aria-controls="schedule_table" rowspan="1" colspan="1" aria-label="Периодичность: активировать для сортировки столбца по возрастанию" style="width: 138px;">Периодичность</th><th class="sorting" tabindex="0" aria-controls="schedule_table" rowspan="1" colspan="1" aria-label="Автобус: активировать для сортировки столбца по возрастанию" style="width: 77px;">Автобус</th><th class="sorting" tabindex="0" aria-controls="schedule_table" rowspan="1" colspan="1" aria-label="Мест: активировать для сортировки столбца по возрастанию" style="width: 34px;">Мест</th><th class="sorting" tabindex="0" aria-controls="schedule_table" rowspan="1" colspan="1" aria-label="АТП: активировать для сортировки столбца по возрастанию" style="width: 143px;">АТП</th><th class="sorting" tabindex="0" aria-controls="schedule_table" rowspan="1" colspan="1" aria-label="Цена: активировать для сортировки столбца по возрастанию" style="width: 35px;">Цена</th></tr>
+		</thead>
+		<tbody>	
+			<tr role="row">
+				<td>05:05</td>
+				<td><a href="/schedule/view.php?station=donetsk_yuzhnyj&amp;trip=1069">АС ДОНЕЦК — ОСТ ГЛИНКА</a></td>
+				<td>06:30</td>
+				<td>пятница</td>
+				<td>МЕРСЕДЕС-16</td>
+				<td>16</td>
+				<td>ЩЕКАТУРОВА ОКСАНА ВИКТОРОВНА ФЛП</td>
+				<td>94.00</td>
+			</tr><tr role="row">
+				<td>05:05</td>
+				<td><a href="/schedule/view.php?station=donetsk_yuzhnyj&amp;trip=1073">АС ДОНЕЦК — ОСТ ГЛИНКА</a>
+				</td>
+				<td>06:30</td>
+				<td>воскресенье</td>
+				<td>БАЗ-28</td>
+				<td>28</td>
+				<td>АФИНЕЦ АЛЕКСАНДР АЛЕКСАНДРОВИЧ ФЛП</td>
+				<td>94.00</td>
+			</tr><tr role="row">
+				<td>05:40</td>
+				<td><a href="/schedule/view.php?station=donetsk_yuzhnyj&amp;trip=7079">АС ДОНЕЦК — АС ДОКУЧАЕВСК</a>
+				</td>
+				<td>06:40</td>
+				<td>ежедневно</td>
+				<td>ИВАН-23</td>
+				<td>23</td>
+				<td>АВТО-ЭКСПРЕСС ООО 12П</td>
+				<td>54.00</td>
+			</tr><tr role="row">
+				<td>05:50</td>
+				<td><a href="/schedule/view.php?station=donetsk_yuzhnyj&amp;trip=17%D0%9C%D0%93">АС ДОНЕЦК — АС АВИЛО-УСПЕНКА АПП</a>
+				</td>
+				<td>08:05</td>
+				<td>ежедневно</td>
+				<td>ФОЛЬЦ 18</td>
+				<td>18</td>
+				<td>ТАРТАЧНЫЙ ВАДИМ ВЛАДИМИРОВИЧ ФЛП</td>
+				<td>130.00</td>
+			</tr><tr role="row" >
+				<td>06:00</td>
+				<td><a href="/schedule/view.php?station=donetsk_yuzhnyj&amp;trip=107%D0%9F">АС ДОНЕЦК — АС МАКЕЕВКА ПЛЕХАНОВО ДС</a>
+				</td>
+				<td>06:45</td>
+				<td>суббота, воскресенье</td>
+				<td>ИВАН-23</td>
+				<td>23</td>
+				<td>ВИКТОРИЯ ООО 10П</td>
+				<td>19.00</td>
+			</tr><tr role="row" class="even">
+				<td>06:00</td>
+				<td><a href="/schedule/view.php?station=donetsk_yuzhnyj&amp;trip=16%D0%90">АС ДОНЕЦК — АС ХАРЦЫЗСК</a></td>
+				<td>07:10</td>
+				<td>ежедневно</td>
+				<td>БАЗ-20</td>
+				<td>20</td>
+				<td>ВИКТОРИЯ ООО 10П</td>
+				<td>41.00</td>
+			</tr><tr role="row">
+				<td>06:00</td>
+				<td><a href="/schedule/view.php?station=donetsk_yuzhnyj&amp;trip=664">АС ДОНЕЦК — АС РОСТОВ</a></td>
+				<td>12:26</td>
+				<td>ежедневно</td>
+				<td>МАН-55</td>
+				<td>55</td>
+				<td>ПОВЕЛКО АННА ЕГОРОВНА ФЛП</td>
+				<td>835.00</td>
+			</tr><tr role="row" class="even">
+				<td>06:05</td>
+				<td><a href="/schedule/view.php?station=donetsk_yuzhnyj&amp;trip=5%D0%9C%D0%A0">АС ДОНЕЦК — АС ЛУГАНСК</a>
+				</td>
+				<td>11:00</td>
+				<td>ежедневно</td>
+				<td>КАВЗ-31</td>
+				<td>31</td>
+				<td>КОНДРАТ ИРИНА ГРИГОРЬЕВНА ФЛП</td>
+				<td>413.00</td>
+			</tr><tr role="row" >
+				<td>06:15</td>
+				<td><a href="/schedule/view.php?station=donetsk_yuzhnyj&amp;trip=7014">АС ДОНЕЦК — АС ДОКУЧАЕВСК</a></td>
+				<td>07:15</td>
+				<td>ежедневно, кроме субботы, воскресенья</td>
+				<td>ИВАН-23</td>
+				<td>23</td>
+				<td>АВТО-ЭКСПРЕСС ООО 12П</td>
+				<td>54.00</td>
+			</tr>
+				</tbody>
+	</table>
     <div>
     <footer>
         <?php include 'futer.php'; ?>
